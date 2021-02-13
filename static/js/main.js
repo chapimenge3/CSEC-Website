81d15cc32464fcc8382defe29d17427323d56166
chapimenge3 <chapimenge3>
2021-02-12 21:36:37 -0500

    Homepage , Contest Done

---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    console.log(scroll);
+    if (scroll > 2000) {
+      $(".navbar").css("background", "#E8DBC3");
+    } else if (scroll > 1425) {
+      $(".navbar").css("background", "#ebece7");
+    } else if (scroll > 500) {
+      $(".navbar").css("background", "#dcd7d3");
+    } else if (scroll > 10) {
+      $(".navbar").css("background", "#e4e0dd");
+    } else {
+      $(".navbar").css("background", "#eeebdf");
+    }
+  });
+});
